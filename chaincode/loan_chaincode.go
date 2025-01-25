780080fe4a0c080d0ef24582e2f408d3c1ab37a3
Prakhar Sharma <prakhars1406@gmail.com>
2025-01-25 10:18:37 +0000

    playground challenge 3 solution

--- chaincode/loan_chaincode.go
+++ chaincode/loan_chaincode.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
@@ -11,34 +12,174 @@ type LoanContract struct {
 }
 
 type Loan struct {
-	LoanID        string   `json:"loanID"`
-	ApplicantName string   `json:"applicantName"`
-	LoanAmount    float64  `json:"loanAmount"`
-	TermMonths    int      `json:"termMonths"`
-	InterestRate  float64  `json:"interestRate"`
-	Outstanding   float64  `json:"outstanding"`
-	Status        string   `json:"status"`
+	LoanID        string    `json:"loanID"`
+	ApplicantName string    `json:"applicantName"`
+	LoanAmount    float64   `json:"loanAmount"`
+	TermMonths    int       `json:"termMonths"`
+	InterestRate  float64   `json:"interestRate"`
+	Outstanding   float64   `json:"outstanding"`
+	Status        string    `json:"status"`
 	Repayments    []float64 `json:"repayments"`
 }
 
 // TODO: Implement ApplyForLoan
 func (c *LoanContract) ApplyForLoan(ctx contractapi.TransactionContextInterface, loanID, applicantName string, loanAmount float64, termMonths int, interestRate float64) error {
+
+	data, err := ctx.GetStub().GetState(loanID)
+	if err != nil {
+		return fmt.Errorf("Failed to retrieve loanID: %v", err)
+	}
+	if data != nil {
+		fmt.Println("loanID already exists")
+		return fmt.Errorf("loanID already exists")
+	}
+
+	if len(loanID) == 0 {
+		fmt.Println("loan Id cannot be null")
+		return errors.New("loan Id cannot be null")
+	}
+
+	if len(applicantName) == 0 {
+		fmt.Println("applicantName cannot be null")
+		return errors.New("applicantName cannot be null")
+	}
+
+	if loanAmount <= 0 {
+		fmt.Println("loanAmount cannot be 0 or less")
+		return errors.New("loanAmount cannot be 0 or less")
+	}
+
+	if termMonths <= 0 {
+		fmt.Println("termMonths cannot be 0 or less")
+		return errors.New("termMonths cannot be 0 or less")
+	}
+
+	if interestRate <= 0 {
+		fmt.Println("interestRate cannot be 0 or less")
+		return errors.New("interestRate cannot be 0 or less")
+	}
+
+	var loandPayload = Loan{
+		LoanID:        loanID,
+		ApplicantName: applicantName,
+		LoanAmount:    loanAmount,
+		TermMonths:    termMonths,
+		InterestRate:  interestRate,
+		Outstanding:   100000 - loanAmount,
+		Status:        "Pending",
+	}
+
+	loanPayloadInByte, err := json.Marshal(loandPayload)
+	if err != nil {
+		fmt.Printf("error in marshalling data %v \n", err)
+		return err
+	}
+
+	err = ctx.GetStub().PutState(loanID, loanPayloadInByte)
+	if err != nil {
+		return fmt.Errorf("Failed to store loan request data: %v", err)
+	}
 	return nil
 }
 
 // TODO: Implement ApproveLoan
 func (c *LoanContract) ApproveLoan(ctx contractapi.TransactionContextInterface, loanID string, status string) error {
+
+	data, err := ctx.GetStub().GetState(loanID)
+	if err != nil {
+		return fmt.Errorf("Failed to retrieve loanID: %v", err)
+	}
+	if data == nil {
+		return fmt.Errorf("loanID not found")
+	}
+
+	var loandPayload Loan
+	err = json.Unmarshal(data, &loandPayload)
+	if err != nil {
+		fmt.Printf("error in unmarshalling loan payload %v \n", err)
+		return err
+	}
+
+	if loandPayload.Status != "Pending" {
+		fmt.Printf("cannot approve non Pending Loans \n")
+		return errors.New("cannot approve non Pending Loans")
+	}
+
+	loandPayload.Status = status
+
+	loanPayloadInByte, err := json.Marshal(loandPayload)
+	if err != nil {
+		fmt.Printf("error in marshalling data %v \n", err)
+		return err
+	}
+
+	err = ctx.GetStub().PutState(loanID, loanPayloadInByte)
+	if err != nil {
+		return fmt.Errorf("Failed to update loan status data: %v", err)
+	}
+
 	return nil
 }
 
 // TODO: Implement MakeRepayment
 func (c *LoanContract) MakeRepayment(ctx contractapi.TransactionContextInterface, loanID string, repaymentAmount float64) error {
+
+	data, err := ctx.GetStub().GetState(loanID)
+	if err != nil {
+		return fmt.Errorf("Failed to retrieve loanID: %v", err)
+	}
+	if data == nil {
+		return fmt.Errorf("loanID not found")
+	}
+
+	var loandPayload Loan
+	err = json.Unmarshal(data, &loandPayload)
+	if err != nil {
+		fmt.Printf("error in unmarshalling loan payload %v \n", err)
+		return err
+	}
+
+	if loandPayload.Status == "Pending" {
+		fmt.Printf("cannot make repayment for Pending Loans \n")
+		return errors.New("cannot make repayment for Pending Loans")
+	}
+
+	loandPayload.Outstanding = loandPayload.Outstanding - repaymentAmount
+	loandPayload.Repayments = append(loandPayload.Repayments, repaymentAmount)
+
+	loanPayloadInByte, err := json.Marshal(loandPayload)
+	if err != nil {
+		fmt.Printf("error in marshalling data %v \n", err)
+		return err
+	}
+
+	err = ctx.GetStub().PutState(loanID, loanPayloadInByte)
+	if err != nil {
+		return fmt.Errorf("Failed to make repayment: %v", err)
+	}
+
 	return nil
 }
 
 // TODO: Implement CheckLoanBalance
 func (c *LoanContract) CheckLoanBalance(ctx contractapi.TransactionContextInterface, loanID string) (*Loan, error) {
-	return nil, nil
+
+	data, err := ctx.GetStub().GetState(loanID)
+	if err != nil {
+		return &Loan{}, fmt.Errorf("Failed to retrieve loanID: %v", err)
+	}
+	if data == nil {
+		return &Loan{}, fmt.Errorf("loanID not found")
+	}
+
+	var loandPayload Loan
+	err = json.Unmarshal(data, &loandPayload)
+	if err != nil {
+		fmt.Printf("error in unmarshalling loan payload %v \n", err)
+		return &Loan{}, err
+	}
+
+	return &loandPayload, nil
 }
 
 func main() {
--- client/client.go
+++ client/client.go
@@ -1,37 +1,120 @@
 package main
 
 import (
+	"crypto/x509"
 	"fmt"
+	"github.com/hyperledger/fabric-gateway/pkg/client"
+	"github.com/hyperledger/fabric-gateway/pkg/identity"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
-
-	"github.com/hyperledger/fabric-sdk-go/pkg/client/ledger"
-	"github.com/hyperledger/fabric-sdk-go/pkg/gateway"
+	"os"
+	"path"
+	"time"
 )
 
-func main() {
-	walletPath := "./wallet"
-	ccpPath := "./connection-org1.yaml"
+const (
+	channelName   = "mychannel"
+	chaincodeName = "loan"
+	mspID         = "Org1MSP"
+	cryptoPath    = "/home/prakhar.sharma@npci.org.in/Documents/fabric/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com"
+	certPath      = cryptoPath + "/users/User1@org1.example.com/msp/signcerts/cert.pem"
+	keyPath       = cryptoPath + "/users/User1@org1.example.com/msp/keystore/"
+	tlsCertPath   = cryptoPath + "/peers/peer0.org1.example.com/tls/ca.crt"
+	peerEndpoint  = "localhost:7051"
+	gatewayPeer   = "peer0.org1.example.com"
+)
 
-	wallet, err := gateway.NewFileSystemWallet(walletPath)
+// newIdentity creates a client identity for this Gateway connection using an X.509 certificate.
+func newIdentity() *identity.X509Identity {
+	certificate, err := loadCertificate(certPath)
 	if err != nil {
-		log.Fatalf("Failed to create wallet: %v", err)
+		panic(err)
 	}
 
-	gw, err := gateway.Connect(
-		gateway.WithConfig(ccpPath),
-		gateway.WithIdentity(wallet, "appUser"),
+	id, err := identity.NewX509Identity(mspID, certificate)
+	if err != nil {
+		panic(err)
+	}
+
+	return id
+}
+
+func loadCertificate(filename string) (*x509.Certificate, error) {
+	certificatePEM, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read certificate file: %w", err)
+	}
+	return identity.CertificateFromPEM(certificatePEM)
+}
+
+// newSign creates a function that generates a digital signature from a message digest using a private key.
+func newSign() identity.Sign {
+	files, err := os.ReadDir(keyPath)
+	if err != nil {
+		panic(fmt.Errorf("failed to read private key directory: %w", err))
+	}
+	privateKeyPEM, err := os.ReadFile(path.Join(keyPath, files[0].Name()))
+
+	if err != nil {
+		panic(fmt.Errorf("failed to read private key file: %w", err))
+	}
+
+	privateKey, err := identity.PrivateKeyFromPEM(privateKeyPEM)
+	if err != nil {
+		panic(err)
+	}
+
+	sign, err := identity.NewPrivateKeySign(privateKey)
+	if err != nil {
+		panic(err)
+	}
+
+	return sign
+}
+
+// newGrpcConnection creates a gRPC connection to the Gateway server.
+func newGrpcConnection() *grpc.ClientConn {
+	certificate, err := loadCertificate(tlsCertPath)
+	if err != nil {
+		panic(err)
+	}
+
+	certPool := x509.NewCertPool()
+	certPool.AddCert(certificate)
+	transportCredentials := credentials.NewClientTLSFromCert(certPool, gatewayPeer)
+
+	connection, err := grpc.Dial(peerEndpoint, grpc.WithTransportCredentials(transportCredentials))
+	if err != nil {
+		panic(fmt.Errorf("failed to create gRPC connection: %w", err))
+	}
+
+	return connection
+}
+
+func main() {
+	clientConnection := newGrpcConnection()
+	defer clientConnection.Close()
+
+	id := newIdentity()
+	sign := newSign()
+
+	gateway, err := client.Connect(
+		id,
+		client.WithSign(sign),
+		client.WithClientConnection(clientConnection),
+		client.WithEvaluateTimeout(5*time.Second),
+		client.WithEndorseTimeout(15*time.Second),
+		client.WithSubmitTimeout(5*time.Second),
+		client.WithCommitStatusTimeout(1*time.Minute),
 	)
 	if err != nil {
-		log.Fatalf("Failed to connect to gateway: %v", err)
+		panic(err)
 	}
-	defer gw.Close()
+	defer gateway.Close()
 
-	network, err := gw.GetNetwork("mychannel")
-	if err != nil {
-		log.Fatalf("Failed to get network: %v", err)
-	}
-
-	contract := network.GetContract("loan")
+	network := gateway.GetNetwork(channelName)
+	contract := network.GetContract(chaincodeName)
 
 	// TODO: Call ApplyForLoan
 	_, err = contract.SubmitTransaction("ApplyForLoan", "loan1", "John Doe", "5000", "12", "5.5")
@@ -40,11 +123,22 @@ func main() {
 	}
 	fmt.Println("Loan successfully applied.")
 
+	_, err = contract.SubmitTransaction("ApproveLoan", "loan1", "Approved")
+	if err != nil {
+		log.Fatalf("Failed to approve loan: %v", err)
+	}
+	fmt.Println("Loan status updated to Approved")
+
+	_, err = contract.SubmitTransaction("MakeRepayment", "loan1", "1000")
+	if err != nil {
+		log.Fatalf("Failed to make repayment for loan: %v", err)
+	}
+	fmt.Println("Repayment recorded. Outstanding balance updated.")
+
 	// TODO: Call CheckLoanBalance
 	result, err := contract.EvaluateTransaction("CheckLoanBalance", "loan1")
 	if err != nil {
 		log.Fatalf("Failed to get loan balance: %v", err)
 	}
-	fmt.Printf("Loan details: %s
-", string(result))
+	fmt.Printf("Loan details: %s", string(result))
 }
